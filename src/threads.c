197a1ceca7b037293980735eeaaa436cdf0c6f9f
Ana Paula Mello <apcomello@gmail.com>
2017-05-07 21:10:20 -0300

    Files preparation

---
+++ src/threads.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../internal.h"
+#include "../support.h"
+#include "../cdata.h"
+#include "../cthread.h"
---
+++ testes/I_am_the_future_test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../internal.h"
+#include "../support.h"
+#include "../cdata.h"
+#include "../cthread.h"
+
+int main(){
+
+  printf("I am the future test!");
+
+  return 0;
+}
