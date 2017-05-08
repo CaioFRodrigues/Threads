89a54b9eae6f77f62f15332e056318e2da172a94
Ana Paula Mello <apcomello@gmail.com>
2017-05-07 22:01:30 -0300

    Simple header documentation

---
+++ include/internal.h
@@ -0,0 +1,17 @@
+/*
+*   Ana Paula Mello - 260723
+*   Arateus Meneses - 242260
+*   Caio Rodrigues -261578
+*
+*   internal.h: funções auxiliares da implementação da interface
+*
+*   versão 1.0 - 07/05/2017
+*/
+
+int free();
+
+int control_thread();
+
+int choose_thread();
+
+int control_blocked();
--- src/threads.c
+++ src/threads.c
@@ -1,3 +1,19 @@
+/*
+*   UFRGS - Universidade Federal do Rio Grande do Sul
+*   Instituto de Informática
+*   INF01142 - Sistemas Operacionais I (2017/1)
+*
+*   Trabalho Prático I - Biblioteca de Threads cthreads 17.1
+*
+*   Ana Paula Mello - 260723 <apcomello@gmail.com>
+*   Arateus Meneses - 242260 <caio_f.r@hotmail.com>
+*   Caio Rodrigues -261578  <arateus.meneses@gmail.com>
+*
+*   threads.c: funções relacionadas à estrutura de threads
+*
+*   versão 1.0 - 07/05/2017
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "../internal.h"
