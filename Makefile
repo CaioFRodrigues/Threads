42c72362126d1687ac70584d9384ab326b9409c5
Caio Rodrigues <caio_f.r@hotmail.com>
2017-05-18 20:21:11 -0300

    Major refactoring done, program executes, ready to start cjoin

--- Makefile
+++ Makefile
@@ -16,25 +16,31 @@ INC_DIR=./include/
 BIN_DIR=./bin/
 SRC_DIR=./src/
 TST_DIR=./testes/
-
+CONDITIONS=-Wall -m32
 all: regra1
 
-regra1: internal.o semaforo.o threads.o testes.o
-	$(CC) -o exec $(BIN_DIR)internal.o $(BIN_DIR)semaforo.o $(BIN_DIR)threads.o $(BIN_DIR)support.o $(BIN_DIR)testes.o -Wall -m32
+regra1: internal.o semaforo.o threads.o testes.o init.o manager.o
+	$(CC) -o exec $(BIN_DIR)internal.o $(BIN_DIR)semaforo.o $(BIN_DIR)init.o $(BIN_DIR)threads.o $(BIN_DIR)/manager.o $(BIN_DIR)support.o $(BIN_DIR)testes.o $(CONDITIONS)
 
 internal.o: $(SRC_DIR)internal.c
-	$(CC) -o $(BIN_DIR)internal.o -c $(SRC_DIR)internal.c -Wall -m32
+	$(CC) -o $(BIN_DIR)internal.o -c $(SRC_DIR)internal.c $(CONDITIONS)
 
 semaforo.o: $(SRC_DIR)semaforo.c
-	$(CC) -o $(BIN_DIR)semaforo.o -c $(SRC_DIR)semaforo.c -Wall -m32
+	$(CC) -o $(BIN_DIR)semaforo.o -c $(SRC_DIR)semaforo.c $(CONDITIONS)
 
 threads.o: $(SRC_DIR)threads.c
-	$(CC) -o $(BIN_DIR)threads.o -c $(SRC_DIR)threads.c -Wall -m32
+	$(CC) -o $(BIN_DIR)threads.o -c $(SRC_DIR)threads.c $(CONDITIONS)
 
+init.o: $(SRC_DIR)init.c
+	$(CC) -o $(BIN_DIR)init.o -c $(SRC_DIR)init.c $(CONDITIONS)
 
-testes.o: $(TST_DIR)test1.c 	#Dunno if the testes is exactly right
-	$(CC) -o $(BIN_DIR)testes.o -c $(TST_DIR)test1.c -Wall -m32
+manager.o: $(SRC_DIR)manager.c
+	$(CC) -o $(BIN_DIR)manager.o -c $(SRC_DIR)manager.c $(CONDITIONS)
+
+testes.o: $(TST_DIR)I_am_the_future_test.c 	#Dunno if the testes is exactly right
+	$(CC) -o $(BIN_DIR)testes.o -c $(TST_DIR)I_am_the_future_test.c $(CONDITIONS)
+
 
 
 clean:
-	rm -rf $(LIB_DIR)/*.a $(BIN_DIR)/internal.o $(BIN_DIR)/semaforo.o $(BIN_DIR)/threads.o $(BIN_DIR)/testes.o $(SRC_DIR)/*~ $(INC_DIR)/*~ *~ 
+	rm -rf $(LIB_DIR)/*.a $(BIN_DIR)/internal.o $(BIN_DIR)/init.o $(BIN_DIR)/manager.o  $(BIN_DIR)/semaforo.o $(BIN_DIR)/threads.o $(BIN_DIR)/testes.o $(SRC_DIR)/*~ $(INC_DIR)/*~ *~ 
--- src/threads.c
+++ src/threads.c
@@ -14,9 +14,7 @@
 *   versão 1.0 - 07/05/2017
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <ucontext.h>
+
 #include "../include/internal.h"
 #include "../include/support.h"
 #include "../include/cdata.h"
@@ -27,7 +25,6 @@ extern TCB_t current_thread;
 extern ucontext_t choose_thread_context; //Contexto sempre começa no choose_thread, então no final de cada thread é necessário ir para lá
 extern FILA2 fila_threads[FILA_SIZE];                        // escolher a próxima
 
-int tidCounter = 1; // Thread Identifier Counter
 int init_flag = 0; // 0 - Biblioteca ainda não foi iniciada; 1 - Biblioteca já foi iniciada
 
 
@@ -38,42 +35,27 @@ int init_flag = 0; // 0 - Biblioteca ainda não foi iniciada; 1 - Biblioteca já
 //Retorna -1 se não conseguir criar uma nova thread, e 0 caso consiga
 int ccreate(void *(*start)(void *), void *arg, int prio)
 {
-    
-    TCB_t *new_thread = (TCB_t *) malloc(sizeof(TCB_t)); //Ponteiro para nova thread que será posto na fila
-    ucontext_t *new_thread_context = (ucontext_t *) malloc(sizeof(ucontext_t)); //Ponteiro do contexto da nova thread 
-
-    if(getcontext(new_thread_context) == -1){ //Pega o contexto atual
-        return -1;
-    }
-    
-
-
     if (init_flag == 0)  //Primeira execução do programa
         init();
     
+    TCB_t * new_thread = create_thread(start, arg, prio);
+    
+    if (new_thread == NULL)
+        return -1;
 
+    insert_thread_in_fila(new_thread);
 
-    new_thread_context->uc_stack.ss_sp = malloc(MEM);
-    new_thread_context->uc_stack.ss_size = MEM;
-    new_thread_context->uc_link = &choose_thread_context;
-    new_thread->state = PROCST_APTO;
-    new_thread->tid = ++tidCounter;
-    makecontext(new_thread_context, (void (*) (void)) start, 1, arg);
-    new_thread->context = *new_thread_context;
+    free_thread();
 
-    insert_thread(prio, new_thread);
-
-    choose_thread();
-
-    return tidCounter;
+    return new_thread->ticket;
 }
 
 // Abdica do controle do processador e vai para a próxima thread
 // retorna 0 se deu erro e -1 se ocorreu falha (não havia thread para trocar).
 int cyield(){
-    if (choose_thread() == 0)
-        return 1;
-    else return -1;
+    if (free_thread() != 0)
+        return -1;
+    else return 0;
 }
 
 
@@ -94,7 +76,7 @@ int csetprio(int tid, int prio){
                 tested_thread->ticket = prio;
                 DeleteAtIteratorFila2(&fila_threads[i]);
                 //Muda a thread para a fila apropriada
-                insert_thread(prio, tested_thread);
+                insert_thread_in_fila(tested_thread);
                 
                 return 0;
             }
