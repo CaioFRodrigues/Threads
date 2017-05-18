14ab8a7b05ca6a60248e60d3f1f0968ddb79dea3
Caio Rodrigues <caio_f.r@hotmail.com>
2017-05-18 20:30:53 -0300

    Changed gitignore and added the files

---
+++ include/manager.h
@@ -0,0 +1,21 @@
+/*
+*   Ana Paula Mello - 260723
+*   Arateus Meneses - 242260
+*   Caio Rodrigues -261578
+*
+*   manager.h: funções auxiliares de manuseamento interno de threads
+*
+*   versão 1.0 - 07/05/2017
+*/
+
+
+#ifndef __MANAGER__H
+#define __MANAGER__H
+
+#include "internal.h"
+
+void end_thread();
+int free_thread();
+
+
+#endif
---
+++ src/manager.c
@@ -0,0 +1,45 @@
+/*
+*   UFRGS - Universidade Federal do Rio Grande do Sul
+*   Instituto de Informática
+*   INF01142 - Sistemas Operacionais I (2017/1)
+*
+*   Trabalho Prático I - Biblioteca de Threads cthreads 17.1
+*
+*   Ana Paula Mello - 260723 <apcomello@gmail.com>
+*   Arateus Meneses - 242260 <arateus.meneses@gmail.com>
+*   Caio Rodrigues -261578  <caio_f.r@hotmail.com>
+*
+*   manager.c: funções auxiliares de manuseamento interno da thread
+*
+*   versão 1.0 - 07/05/2017
+*/
+
+
+#include "../include/manager.h"
+
+
+//Função control_thread(): Manuseia próxima thread a ser executada e checa a fila de bloqueios
+void end_thread(){	
+
+
+	//Decide a próxima thread a ser executada
+	TCB_t * next_thread = get_next_thread();
+	current_thread = *next_thread;
+	setcontext(&(current_thread.context));
+
+	
+}
+
+//Função free_thread(): Coloca a thread atual na fila de threads, e coloca a primeira thread da fila em execução 
+int free_thread(){
+	TCB_t * next_thread = get_next_thread();
+	
+	if (next_thread == NULL)
+		return -1;
+
+	update_current_thread(next_thread);
+
+	swap_context(next_thread);
+
+	return 0;
+}
